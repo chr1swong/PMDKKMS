64f94cb5776a3f52b3ab8ddb04e2db006fc4f4e2
Christopher Wong <chriswong0839@gmail.com>
2024-12-09 16:39:39 +0800

    testing index.php for reliability issue 1

--- pmdkkms/public/index.php
+++ pmdkkms/public/index.php
@@ -6,7 +6,7 @@ define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require_once $maintenance;
+    require $maintenance;
 }
 
 // Register the Composer autoloader...
